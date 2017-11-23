cd10e390d598b4150b478c8b54d47c3767c08f21
Jenil Gopani <jenil@webmavens.com>
2017-11-23 16:36:35 +0530

    add tesseract library

--- app/src/main/java/com/scanner/demo/MainActivity.java
+++ app/src/main/java/com/scanner/demo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.scanner.demo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,19 +13,25 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
+import com.googlecode.tesseract.android.TessBaseAPI;
 import com.scanlibrary.ScanActivity;
 import com.scanlibrary.ScanConstants;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
 public class MainActivity extends ActionBarActivity {
 
-
-    private Button cameraButton;
+    Bitmap bitmap = null;
+    private Button cameraButton,scan;
     private ImageView scannedImageView;
     int REQUEST_CODE = 99;
+    private TessBaseAPI mTess;
+    String datapath = "";
     int preference = ScanConstants.PICKFILE_REQUEST_CODE;
 
     @Override
@@ -31,6 +40,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         cameraButton = (Button)findViewById(R.id.cameraButton);
+        scan = (Button)findViewById(R.id.scan);
         scannedImageView = (ImageView) findViewById(R.id.scannedImage);
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,8 +51,91 @@ public class MainActivity extends ActionBarActivity {
 
             }
         });
+
+
+        String language = "eng";
+        datapath = getFilesDir()+ "/tesseract/";
+        mTess = new TessBaseAPI();
+        checkFile(new File(datapath + "tessdata/"));
+
+        mTess.init(datapath, language);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String OCRresult = null;
+                mTess.setImage(bitmap);
+                OCRresult = mTess.getUTF8Text();
+                //Toast.makeText(getApplicationContext(), OCRresult ,Toast.LENGTH_SHORT).show();
+
+                final AlertDialog alertDialog = new AlertDialog.Builder(
+                        MainActivity.this).create();
+
+                // Setting Dialog Title
+                alertDialog.setTitle("OCR Text");
+
+                // Setting Dialog Message
+                alertDialog.setMessage(OCRresult);
+
+                // Setting OK Button
+                alertDialog.setButton("Done", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        alertDialog.cancel();
+                    }
+                });
+
+                // Showing Alert Message
+                alertDialog.show();
+
+
+            }
+        });
+
+
     }
 
+    private void checkFile(File dir) {
+        //directory does not exist, but we can successfully create it
+        if (!dir.exists() && dir.mkdirs()) {
+            copyFiles();
+        }
+        //The directory exists, but there is no data file in it
+        if (dir.exists()) {
+            String datafilepath = datapath + "/tessdata/eng.traineddata";
+            File datafile = new File(datafilepath);
+            if (!datafile.exists()) {
+                copyFiles();
+            }
+        }
+    }
+
+    private void copyFiles() {
+        try {
+            //location we want the file to be at
+            String filepath = datapath + "/tessdata/eng.traineddata";
+
+            //get access to AssetManager
+            AssetManager assetManager = getAssets();
+
+            //open byte streams for reading/writing
+            InputStream instream = assetManager.open("tessdata/eng.traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+
+            //copy the file to the location specified by filepath
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     @Override
@@ -50,7 +143,7 @@ public class MainActivity extends ActionBarActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             Uri uri = data.getExtras().getParcelable(ScanConstants.SCANNED_RESULT);
-            Bitmap bitmap = null;
+
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
                 getContentResolver().delete(uri, null, null);
@@ -60,5 +153,6 @@ public class MainActivity extends ActionBarActivity {
             }
         }
     }
+
 
 }
